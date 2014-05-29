d6aae4e33b9c8cafcfe77840f2b385bed46cd51f
Ahsan <vampxlr@yahoo.com>
2014-05-30 03:17:59 +0600

    correction

--- Freelance.AuditApp/Data.Designer.cs
+++ Freelance.AuditApp/Data.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'J:\Git\Freelance.AuditApp\Freelance.AuditApp\Data.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Ahsan\Desktop\Freelance AppTaskManagement\Freelance.AuditApp - SPA\Freelance.AuditApp\Freelance.AuditApp\Data.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
