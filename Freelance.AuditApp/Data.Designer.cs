b8193fec499eeb01760b467053077af57d7293af
Ahsan <vampxlr@yahoo.com>
2014-05-29 16:50:28 +0600

    first Commit

---
+++ Freelance.AuditApp/Data.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\Users\awais_000\Desktop\Fiverr\landsea\Code\Freelance.AuditApp\Data.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
